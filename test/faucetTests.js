a56dcbd72730873cbd5c1646542b636966d00082
Rick Steawart <rickstewartdev@gmail.com>
2023-11-21 18:24:24 +0000

    end of project

--- test/faucetTests.js
+++ test/faucetTests.js
@@ -1,5 +1,6 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Faucet", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -9,15 +10,44 @@ describe("Faucet", function () {
     const Faucet = await ethers.getContractFactory("Faucet");
     const faucet = await Faucet.deploy();
 
-    const [owner] = await ethers.getSigners();
+    const [owner, notOwner] = await ethers.getSigners();
 
-    console.log("Signer 1 address: ", owner.address);
-    return { faucet, owner };
+    let withdrawAmount = ethers.parseUnits("1", "ether");
+
+    return { faucet, owner, withdrawAmount, notOwner };
   }
 
   it("should deploy and set the owner correctly", async function () {
     const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
 
     expect(await faucet.owner()).to.equal(owner.address);
+  });
+
+  it("should not allow withdrawals above .1 ETH at a time", async function () {
+    const { faucet, withdrawAmount } = await loadFixture(
+      deployContractAndSetVariables
+    );
+    await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
+  });
+
+  it("should only allow owner to call destroyFaucet", async function () {
+    const { faucet, notOwner } = await loadFixture(
+      deployContractAndSetVariables
+    );
+    await expect(faucet.connect(notOwner).destroyFaucet()).to.be.reverted;
+  });
+
+  it("should only allow owner withdrawAll", async function () {
+    const { faucet, notOwner } = await loadFixture(
+      deployContractAndSetVariables
+    );
+    await expect(faucet.connect(notOwner).withdrawAll()).to.be.reverted;
+  });
+
+  it("should empty walet with withdrawAll", async function () {
+    const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
+    await faucet.connect(owner).withdrawAll();
+    const balance = await ethers.provider.getBalance(faucet.getAddress());
+    expect(balance).to.equal(0);
   });
 });
